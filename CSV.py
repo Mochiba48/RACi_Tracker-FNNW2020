493e203980abf2eb2e208c699195afc24ae4575a
fuch94 <63659261+fuch94@users.noreply.github.com>
2020-04-18 11:56:59 +0200

    Kategorien erstellen
    hat noch nicht geklappt

--- CSV.py
+++ CSV.py
@@ -1,12 +1,21 @@
 import csv
 
 
-#in csv importieren:
+#in csv aufgabe importieren:
 def aufgaben_listeinp(daten):
     with open('Aufgaben.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for line in csv_reader:
             print(line)
+
+#in csv Kategorie importieren:
+def kategorie_listeinp(daten):
+    with open('Kategorie.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        daten = []
+        for line in csv_reader:
+            daten.append(line)
+        print(daten)
 
 
 #in csv aufgabe exportieren:
@@ -15,14 +24,18 @@ def aufgaben_listeexp(daten):
         aufgaben_writer = csv.writer(aufgaben_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         aufgaben_writer.writerow(daten)
 
+#in csv Kategorie exportieren:
+def kategorie_listeexp(daten):
+    with open('Kategorie.csv', mode='a', newline='') as kategorie_file:
+        kategorie_writer = csv.writer(kategorie_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        kategorie_writer.writerow(daten)
+
 
 #in csv alles exportieren (wird alles überschrieben):
 def aufgaben_listeexpkompl(daten):
     with open('Aufgaben.csv', mode='w', newline='') as aufgaben_file:
         aufgaben_writer = csv.writer(aufgaben_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         aufgaben_writer.writerow(daten)
-
-
 
 
 
